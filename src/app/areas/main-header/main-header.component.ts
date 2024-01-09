43ff525850732890cd85fe9ba1f6de84906f815d
Maciej Lipiński <maciej@statlook.com>
2024-01-09 13:16:27 +0100

    preparing for nest js - header

--- src/app/areas/main-header/main-header.component.ts
+++ src/app/areas/main-header/main-header.component.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { LoginService } from 'src/app/areas/login/login.service';
 
 @Component({
-    selector: 'main-header',
-    templateUrl: './main-header.component.html',
-    styleUrls: ['./main-header.component.scss']
+  selector: 'main-header',
+  templateUrl: './main-header.component.html',
+  styleUrls: ['./main-header.component.scss']
 })
 
 export class MainHeaderComponent implements OnInit {
 
-    constructor(
-        public LoginService: LoginService
-    ) { }
+  constructor(
+    private router: Router,
+    public LoginService: LoginService
+  ) { }
 
-    ngOnInit(): void {
-        
-    }
+  ngOnInit(): void {
 
+  }
+
+  OnLogoClick() {
+    this.router.navigate(['/']);
+  }
 }
