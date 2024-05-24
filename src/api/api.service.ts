145d56a458403d8856431a64552b21f083eb4f71
Maciej Lipiński <maciej@statlook.com>
2024-05-24 11:50:03 +0200

    formatting - scan - api

--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -1,57 +1,60 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class ApiService {
-  constructor() { }
+    constructor() {}
 
-  async sendGPTPrompt(prompt: string) {
-    const gpt4: boolean = true;
-    const openAIAPIKey: string = './api.keys';
-    const bearerKey: string = 'Bearer ' + openAIAPIKey;
+    async sendGPTPrompt(prompt: string) {
+        const gpt4: boolean = true;
+        const openAIAPIKey: string = './api.keys';
+        const bearerKey: string = 'Bearer ' + openAIAPIKey;
 
-    if (gpt4) {
-      const url = 'https://api.openai.com/v1/chat/completions';
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': bearerKey
-        },
-        body: JSON.stringify({
-          model: 'gpt-4-turbo-preview',
-          messages: [{
-            role: 'user',
-            content: prompt
-          }],
-          max_tokens: 4000,
-          temperature: 0.2
-        })
-      });
-      const data = await response.json();
-      return data;
+        if (gpt4) {
+            const url = 'https://api.openai.com/v1/chat/completions';
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: bearerKey,
+                },
+                body: JSON.stringify({
+                    model: 'gpt-4-turbo-preview',
+                    messages: [
+                        {
+                            role: 'user',
+                            content: prompt,
+                        },
+                    ],
+                    max_tokens: 4000,
+                    temperature: 0.2,
+                }),
+            });
+            const data = await response.json();
+            return data;
+        } else {
+            const url = 'https://api.openai.com/v1/completions';
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: bearerKey,
+                },
+                body: JSON.stringify({
+                    model: 'gpt-3.5-turbo',
+                    messages: [
+                        {
+                            role: 'user',
+                            content: prompt,
+                        },
+                    ],
+                    max_tokens: 4000,
+                    temperature: 0.2,
+                }),
+            });
+            const data = await response.json();
+            return data;
+        }
     }
-    else {
-      const url = 'https://api.openai.com/v1/completions';
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': bearerKey
-        },
-        body: JSON.stringify({
-          model: 'gpt-3.5-turbo',
-          messages: [{
-            role: 'user',
-            content: prompt
-          }],
-          max_tokens: 4000,
-          temperature: 0.2
-        })
-      });
-      const data = await response.json();
-      return data;
-    }
-  }
 }
